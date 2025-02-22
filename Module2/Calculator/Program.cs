a88ba310c17b26742d2ad55eda1bb4048be43e8e
pjsm <pjsm.lesi@gmail.com>
2025-02-22 23:35:41 +0000

    UPDATE Program.cs

--- Module2/Calculator/Program.cs
+++ Module2/Calculator/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Calculator {
+    public static int number1;
+    public static int number2;
+
+    public static int Add() {
+        return number1 + number2;
+}
+
+    static void Main(string[] args) {
+        number1 = 5;
+        number2 = 10;
+
+        int result = Add();
+        Console.WriteLine("Sum: " + result);
+}
+}
+
