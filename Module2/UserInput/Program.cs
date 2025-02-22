3272e0f573843b9d67c2276b0e2764ae60b27be6
pjsm <pjsm.lesi@gmail.com>
2025-02-22 23:54:41 +0000

    CREATE UserInput project

---
+++ Module2/UserInput/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class UserInput {
+    static void GreetUser() {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello, " + name + "!");
+    }
+
+    static void Main() {
+        GreetUser();
+    }
+}
