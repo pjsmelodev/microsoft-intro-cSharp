9621213464305afe7122428643ff58449d4be7cb
pjsm <pjsm.lesi@gmail.com>
2025-02-22 23:43:05 +0000

    CREATE NumberDisplay project

---
+++ Module2/NumberDisplay/Program.cs
@@ -0,0 +1,11 @@
+﻿public class NumberDisplay {
+    public static void DisplayNumbers() {
+        for (int i = 1; i <=10; i++) {
+           Console.WriteLine(i);
+        }
+    }
+
+    static void Main(string[] args) {
+        DisplayNumbers();
+    }
+}
