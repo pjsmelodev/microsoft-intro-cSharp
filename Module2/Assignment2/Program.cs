172b5035705b3a6cde6146518506401242183e4a
pjsm <pjsm.lesi@gmail.com>
2025-02-24 17:24:27 +0000

    CREATE Assignment2

---
+++ Module2/Assignment2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Step 3: Creating a Loop to Display Numbers
+public class NumberDisplay {
+
+    public static void DisplayNumbers() {
+        for (int i = 1; i <= 10; i++) {
+            Console.WriteLine(i);
+        }
+    }
+
+    public static void Main(string[] args) {
+        DisplayNumbers();
+    }
+}
