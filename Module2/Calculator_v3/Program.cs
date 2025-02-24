0b6dfdde769a8487d876b179ec97741f70a0de57
pjsm <pjsm.lesi@gmail.com>
2025-02-24 17:08:58 +0000

    CREATE Calculator_v3

---
+++ Module2/Calculator_v3/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Calculator {
+    public static int number1 = 13;
+    public static int number2 = -2;
+
+    public static int Add() {
+        return number1 + number2;
+    }
+
+    public static void Main(string[] args) {
+        int result = Add();
+        Console.WriteLine(result);
+    }
+}
