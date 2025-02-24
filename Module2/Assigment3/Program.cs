e7d94844a224ca42234b10a513b5d2fa40af1023
pjsm <pjsm.lesi@gmail.com>
2025-02-24 17:31:15 +0000

    CREATE Assignment3

---
+++ Module2/Assigment3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Step 4: Handling User Input
+public class UserInput {
+    public static void GreetUser() {
+        Console.WriteLine("Name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello " + name + "!");
+
+    }
+
+    public static void Main(string[] args) {
+        GreetUser();
+    }
+}
