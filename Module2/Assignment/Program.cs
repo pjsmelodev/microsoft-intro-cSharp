2aa789513d8d01ae801cb1bc762d6bf27a910538
pjsm <pjsm.lesi@gmail.com>
2025-02-24 17:19:13 +0000

    CREATE Assigment

---
+++ Module2/Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿// Step 1: Creating a Simple Calculator
+public class Calculator {
+    public static int number1, number2;
+
+    public static int Add() {
+        return number1 + number2;
+    }
+
+// Step 2: Executing the Calculator Program
+    public static void Main(string[] args) {
+        number1 = 10;
+        number2 = -3;
+
+        Console.WriteLine(Add());
+    }
+}
