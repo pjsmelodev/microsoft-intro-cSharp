533c4b0cc7e80d5ee7a5a18942430363cc2a0288
pjsm <pjsm.lesi@gmail.com>
2025-02-24 17:02:44 +0000

    CREATE GreetUser

---
+++ Module2/GreetUser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class UserInput {
+    public static void GreetUser() {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello " + name + "!");
+    }
+
+    public static void Main(string[] args) {
+        GreetUser();
+    }
+}
