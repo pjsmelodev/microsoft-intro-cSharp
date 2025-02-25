d1df8798264a21b4973eadda1bae585ed53893b6
pjsm <pjsm.lesi@gmail.com>
2025-02-25 19:04:11 +0000

    CREATE GuidedLab

---
+++ Module3/GuidedLab/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Animal {
+    public virtual void MakeSound() {
+        Console.WriteLine("Some generic animal sound.");
+    }
+}
+
+public class Dog : Animal {
+    public override void MakeSound()
+    {
+        Console.WriteLine("Woof!");
+    }
+}
+
+public class Cat : Animal {
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow!");
+    }
+}
+
+class Program {
+    static void Main(string[] args) {  // 🔹 Método Main dentro de uma classe
+        Dog dog = new Dog();
+        dog.MakeSound();  // Output: Woof!
+
+        Cat cat = new Cat();
+        cat.MakeSound();  // Output: Meow!
+    }
+}
