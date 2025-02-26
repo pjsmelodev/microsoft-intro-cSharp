06053ace0d5c7cea3cb47fccba03c888a909d1fb
pjsm <pjsm.lesi@gmail.com>
2025-02-26 16:25:31 +0000

    CREATE People

---
+++ Module3/People/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Person {
+    // Properties
+    public string Name {get; set;}
+    public int Age {get; set;}
+
+    // Methods
+    public void Greet(string name, int age) {
+        Name = name;
+        Age = age;
+        Console.WriteLine($"Hi {Name}! You're {Age}, right?");
+    }
+}
+
+public class Program {
+    public static void Main(string[] args) {
+        Person friend = new Person();
+        friend.Name = "José";
+        friend.Age = 55;
+
+        Person colleague = new Person();
+        colleague.Name = "Aristides";
+        colleague.Age = 103;
+
+        friend.Greet(friend.Name, friend.Age);
+        colleague.Greet(colleague.Name, colleague.Age);
+    }
+}
