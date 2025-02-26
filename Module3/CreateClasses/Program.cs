b0487a5d4d5d74eab6e06c56105893ff1f523636
pjsm <pjsm.lesi@gmail.com>
2025-02-26 16:12:23 +0000

    CREATE CreateClasses

---
+++ Module3/CreateClasses/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Person {
+    public string Name {get; set;}
+    public int Age {get; set;}
+
+    //public Person() {}
+    // É preciso criar um constructor se quiser instanciar a classe sem parâmetros (linha 20)
+
+    public Person(string name, int age) {
+        Name = name;
+        Age = age;
+    }
+
+    public void DisplayInfo() {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+
+    public static void Main(string[] args) {
+        Person person = new Person("Paulo", 39);
+        /*
+        Person person = new Person();
+        person.Name = "Paulo";
+        person.Age = 39;
+        */
+        person.DisplayInfo();
+
+        Person neighbor = new Person("José", 53);
+        neighbor.DisplayInfo();
+        neighbor.Age = 52;
+        neighbor.DisplayInfo();
+    }
+}
