b193da925c3c6773729089fb87749f9d5913d4fb
pjsm <pjsm.lesi@gmail.com>
2025-02-27 18:47:56 +0000

    CREATE AsynchMethods

---
+++ Module4/AsyncMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public async Task GetDataAsync() {
+    var data = await GetDataFromAPI();
+    // await -> pausa a execução do método até que a tarefa termine
+    Console.WriteLine(data);
+}
+
+public class Program {
+    public static void Main(string[] args) {
+        
+    }
+}
