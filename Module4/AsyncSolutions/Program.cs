1f40b9fce56c3c35d8431a6db4319ca7855785e4
pjsm <pjsm.lesi@gmail.com>
2025-03-02 19:46:34 +0000

    CREATE AsynchSolutions

---
+++ Module4/AsyncSolutions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Product {
+    public string Name { get; set; }
+
+    public Product(string name) {
+        Name = name;
+    }
+}
+
+public class Program {
+    public async Task<List<Product>> FetchProductsAsync() {
+        await Task.Delay(2000);
+        return new List<Product> {
+            new Product("Eco Bag"),
+            new Product("Resusable Straw")
+        };
+    }
+
+    public async Task DisplayProductAsync() {
+        List<Product> products = await FetchProductsAsync();
+        foreach (Product product in products)
+        {
+            Console.WriteLine(product.Name);
+        }
+    }
+
+    public static async Task Main(string[] args) {
+        Program program = new Program();
+        await program.DisplayProductAsync();
+    }
+}
